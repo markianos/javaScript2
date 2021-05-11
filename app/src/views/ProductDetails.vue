1b38f641e42582b6065dbaf06650e05b47413cec
Alexander Markianose <markianos.alexander@gmail.com>
2021-05-11 13:20:27 +0200

    css Product details

--- app/src/views/ProductDetails.vue
+++ app/src/views/ProductDetails.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!-- details {{ id }}  lägger till id på vår sida i html template-->
-        <div class="container my-5 py-5 z-d-1">
+        <div class="container my-5 py-5">
             
             <section class="text-center">
                 <h3 class="mb-5 font-weight-bold">Product Details</h3>
@@ -9,11 +9,22 @@
                 <div class="row">
                 
                     <div class="col-lg-6">
-                        <img :src="product.image" alt="" class="img-fluid">
+                        <img :src="product.image" alt="" class="img-fluid mt-5">
                     </div>
                 
                     <div class="col-lg-6 text-center text-lg-start">
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, corporis?</p>
+                        <h2 class="text-left font-weight-bold mb-5">{{ product.name }}</h2>
+
+                        <div class="mb-5">
+                            <h5 class="mb-3">Description</h5>
+                            <p>{{ product.desc }}</p>
+                        </div>
+                        
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h3 class="text-danger">{{ product.price }}:-</h3>
+                            <button class=" btn btn-primary btn-sm">Add to Cart</button>
+                        </div>
+                        
                     </div>
 
                 </div>
