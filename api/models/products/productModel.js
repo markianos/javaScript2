523c547f7f6ca825a5063da337d3cbc12ee839d2
Alexander Markianose <markianos.alexander@gmail.com>
2021-05-20 11:40:55 +0200

    mongodb working, postman working, shema + model + controller for product & getProduct function

--- api/models/products/productModel.js
+++ api/models/products/productModel.js
@@ -20,8 +20,8 @@ exports.getProducts = (req, res) => {  // en funktion som hämtar alla våra pro
 }
 
 exports.getProduct = (req, res) => { 
-    Product.exists({ _id: req.params.id }, (err, result) => {  //söker efter _id i mongodb och filtrerar efter den request vi sökt på
-        if(err) {
+    Product.exists({ _id: req.params.id }, (err, result) => {  //söker efter _id i mongodb och filtrerar efter den request vi sökt på och om den existerar
+        if(err) {       // en if sats som plockar upp att det inte gick att genomföra hämtningen/funktionen / anändaren gjorde något fel
             return res.status(400).json({
                 statusCode: 400,
                 status: false,
@@ -29,19 +29,19 @@ exports.getProduct = (req, res) => {
             })
         }
 
-        if(result) {
-            Product.findById(req.params.id)
-              .then(product => res.status(200).json(product))
-              .catch(err => res.status(500).json({
+        if(result) {    // om vi får ett resultat oavsett om det är true eller false så..
+            Product.findById(req.params.id) // använder sig av findById som söker efter id och inte skapar en array går även använda findOne och söka efter något annat än id
+              .then(product => res.status(200).json(product)) // vi får tillbaka vår produkt på ett respons på 200
+              .catch(err => res.status(500).json({  // om error uppstår skickas 500 tillbaka med err.message ex. serverfel
                 statusCode: 500,
                 status: false,
                 message: err.message
               }))
-          } else {
+          } else {      // annars om produkt inte existerar skickas en 404 med err medelande eller vårt meddelande
             res.status(404).json({
               statusCode: 404,
               status: false,
-              message: err || 'Oops, this products does not exist'
+              message: err || 'Oops, this products does not exist' // felmeddelande
             })
           }
         })
